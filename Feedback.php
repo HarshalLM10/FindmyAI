7737c2c5b6dc9af2bc982e596a65de6ae5cf00a7
Harshal Choudhary <harshalchoudhary53@gmail.com>
2024-03-12 16:24:26 +0530

    Connected Feedback form with mysql

--- Feedback.php
+++ Feedback.php
@@ -1,4 +1,41 @@
 
+<?php
+$insert = false;
+if (isset($_POST['name'])) {
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "findmyai";  // Replace with your actual database name
+
+    $con = mysqli_connect($server, $username, $password);
+
+    if (!$con) {
+        die("Connection to this database failed due to " . mysqli_connect_error());
+    }
+
+    // Select the database
+    if (!mysqli_select_db($con, $dbname)) {
+        die("Database selection failed: " . mysqli_error($con));
+    }
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $feedback_desc = $_POST['desc'];  // Change 'desc' to 'feedback_desc' or any suitable name
+
+    $sql = "INSERT INTO feedback (`Name`, `email`, `feedback`, `Timestam`) VALUES ('$name', '$email', '$feedback_desc', current_timestamp())";
+
+    if ($con->query($sql) == true) {
+        $insert = true;
+    } else {
+        // Handle the error if needed
+    }
+
+    $con->close();
+}
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,10 +46,12 @@
     integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <title>Suggestions </title>
-    <style> .bn {
-        background-image: url("2_20240226_113714_0001.png");
-        background-position: center;
-        background-size: cover; /* Set to 'cover' to make the image cover the whole area */
+    <style> 
+    body{
+        background-color: black;
+        color: white;
+    }
+    .bn {
         height: 100vh; 
         width: 100%; 
     }
@@ -21,13 +60,26 @@
         text-align: center;
         padding-top: 50px;
     }
+    .Submitmsg{
+    color: white;
+    font-size: 25px;
+}
     </style>
 </head>
 <body>
+<?php
+                if ($insert == true){
+                echo "<p class='Submitmsg'>Thanks for yoyr valuable feedback.</p>";
+                }
+        ?>
     <div class="container-fluid bn">
-        <h1>Feedback</h1><br>
+        <div class="container text-center mx-auto" id="otherpageh2">
+            <h2>Feedback </h2>
+          </div><br>
         <div class="container su">
-          <form action="" id="feedbackForm">
+
+            
+          <form action="Feedback.php" method="post" id="feedbackForm">
             <input type="text" name="name" id="name" placeholder="Enter your name">
             <input type="email" name="email" id="email" placeholder="Enter your email">
             <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Enter Feedback"></textarea>
